211777831c4b79d88ce5660cd73c15103d6bcd4f
Jiehao Luo <powerseed5044@gmail.com>
2024-02-07 20:18:58 -0700

    draw birds and foods on a canvas

--- libs/simulation-wasm/src/lib.rs
+++ libs/simulation-wasm/src/lib.rs
@@ -1,6 +1,52 @@
+use rand::{Rng, RngCore};
+use serde::Serialize;
+use wasm_bindgen::JsValue;
 use wasm_bindgen::prelude::wasm_bindgen;
+#[derive(Serialize)]
+pub struct World {
+    birds: Vec<Bird>,
+    foods: Vec<Food>
+}
+impl World {
+    pub fn new() -> Self {
+        let mut rng = rand::thread_rng();
 
+        let birds = (0..10).map(|_| Bird::new(&mut rng)).collect();
+        let foods = (0..20).map(|_| Food::new(&mut rng)).collect();
+
+        Self {
+            birds,
+            foods
+        }
+    }
+}
+#[derive(Serialize)]
+pub struct Bird {
+    position: nalgebra::Point2<f32>,
+    rotation: f32,
+    speed: f32
+}
+impl Bird {
+    pub fn new(rng: &mut dyn RngCore) -> Self {
+        Self {
+            position: rng.gen(),
+            rotation: rng.gen::<nalgebra::Rotation2<f32>>().angle(),
+            speed: rng.gen()
+        }
+    }
+}
+#[derive(Serialize)]
+pub struct Food {
+    position: nalgebra::Point2<f32>
+}
+impl Food {
+    pub fn new(rng: &mut dyn RngCore) -> Self {
+        Self {
+            position: rng.gen()
+        }
+    }
+}
 #[wasm_bindgen]
-pub fn whos_that_dog() -> String {
-    "whos_that_dog".into()
+pub fn create_new_world() -> Result<JsValue, JsValue> {
+    Ok(serde_wasm_bindgen::to_value(&World::new())?)
 }
--- www/index.js
+++ www/index.js
@@ -1,5 +1,42 @@
 import * as wasm from "hello-wasm-pack";
 import * as sim from "simulation-wasm";
 
-wasm.greet();
-alert("who's that dog? " + sim.whos_that_dog() + "!");
+const viewport = document.getElementById("viewport");
+const world = sim.create_new_world();
+const ctxt = viewport.getContext('2d');
+
+draw_birds();
+draw_foods();
+function draw_birds() {
+    for (const bird of world.birds) {
+        console.log(bird.rotation)
+        draw_triangle(
+            bird.position[0] * viewport.width,
+            bird.position[1] * viewport.height,
+            bird.rotation,
+            15)
+    }
+}
+
+function draw_triangle(x, y, rotation, size) {
+    ctxt.beginPath();
+
+    ctxt.moveTo(x - Math.sin(rotation) * size * 1.5, y + Math.cos(rotation) * size * 1.5);
+    ctxt.lineTo(x - Math.sin(rotation + 2.0 / 3.0 * Math.PI) * size, y + Math.cos(rotation + 2.0 / 3.0 * Math.PI) * size);
+    ctxt.lineTo(x - Math.sin(rotation + 4.0 / 3.0 * Math.PI) * size, y + Math.cos(rotation + 4.0 / 3.0 * Math.PI) * size);
+    ctxt.lineTo(x - Math.sin(rotation) * size * 1.5, y + Math.cos(rotation) * size * 1.5);
+
+    ctxt.fill();
+}
+
+function draw_foods() {
+    for (const food of world.foods) {
+        ctxt.beginPath();
+        ctxt.arc(food.position[0] * viewport.width, food.position[1] * viewport.height, 4, 0, 2 * Math.PI);
+        ctxt.fill();
+    }
+}
+
+function redraw() {
+
+}
